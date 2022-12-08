0ac3c8a05232695e57d326019cce88da63d8d461
bayram <bayramk@gmail.com>
2022-12-08 10:45:07 +0300

    zoom

--- script/zoom-install.sh
+++ script/zoom-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O /tmp/zoom.deb https://zoom.us/client/latest/zoom_amd64.deb
+wget -O /tmp/zoom.deb https://zoom.us/client/5.12.9.367/zoom_amd64.deb
 dpkg --force-all -i /tmp/zoom.deb
 sleep 1
 rm /tmp/zoom.deb
