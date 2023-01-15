ce9f70329a6d56a4b946da234ba66a617bfd78d6
bayramkarahan <bayramk@gmail.com>
2023-01-15 12:47:32 +0300

    e-ag

--- script/e-ag-client-faz3-install.sh
+++ script/e-ag-client-faz3-install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-apt install e-ag-client e-ag-x11client
+wget -O /tmp/e-ag-client.deb https://github.com/bayramkarahan/e-ag-client/raw/master/e-ag-client_2.1.0_amd64.deb
+dpkg --force-all -i /tmp/e-ag-client.deb
+sleep 1
+wget -O /tmp/e-ag-x11client.deb https://github.com/bayramkarahan/e-ag-x11client/raw/master/e-ag-x11client_2.1.0_amd64.deb
+dpkg --force-all -i /tmp/e-ag-x11client.deb
+sleep 1
 mkdir /var/lib/betikyukleyici
 touch /var/lib/betikyukleyici/e-client
 exit 0
--- script/e-ag-server-faz3-install.sh
+++ script/e-ag-server-faz3-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-apt install e-ag
+wget -O /tmp/e-ag.deb https://github.com/bayramkarahan/e-ag/raw/master/e-ag_2.1.0_amd64.deb
+dpkg --force-all -i /tmp/e-ag.deb
+sleep 1
 mkdir /var/lib/betikyukleyici
 touch /var/lib/betikyukleyici/e-ag
 exit 0
