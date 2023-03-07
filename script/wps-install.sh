b001db290124dccd30d8774b3ee174f3ce7eaecd
bayramkarahan <44923603+bayramkarahan@users.noreply.github.com>
2023-03-07 12:43:17 +0300

    Update wps-install.sh

--- script/wps-install.sh
+++ script/wps-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 wget -O /tmp/wps.deb https://depo.pardus.org.tr/etap/pool/non-free/w/wps-office/wps-office_10.1.0.6757pardus2_amd64.deb
 dpkg --force-all -i /tmp/wps.deb
+wget -O /tmp/wps-tr.deb https://github.com/bayramkarahan/wps-office-tr-dil/blob/master/wps-tr.deb
+dpkg --force-all -i /tmp/wps-tr.deb
 sleep 1
 rm /tmp/wps.deb
 mkdir /var/lib/betikyukleyici
