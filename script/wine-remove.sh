6c0c550a594351dd9777dc84a94afe185250af0a
bayramkarahan <bayramk@gmail.com>
2023-03-23 11:05:46 +0300

    wine-reset

--- script/wine-remove.sh
+++ script/wine-remove.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt remove winemimetype -y
 rm -rf /var/lib/betikyukleyici/winemimetype
-rm -rf /var/lib/betikyukleyici/wine7.0
+rm -rf /var/lib/betikyukleyici/wine
 apt remove wine* -y
 apt --purge remove wine* -y
 apt autoremove -y
