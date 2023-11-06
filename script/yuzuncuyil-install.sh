24fa1a76a1a52acb39ac1b3e8d2f3e52ff9b600d
bayramkarahan <bayramk@gmail.com>
2023-11-06 14:44:03 +0300

    yuzuncuyil

---
+++ script/yuzuncuyil-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#merhaba bu flash ekleme betiği
+#flash dosyaları oynatır.
+
+apt install --fix-missing -y
+apt --fix-broken install -y
+apt-get install -f -y # eksik bağımlılıkları tamamlaması için.
+apt autoremove -y
+apt autoremove -y
+apt update
+############################################################
+wget -O /tmp/yuzuncuyil.deb http://depo.pardus.org.tr/pardus/pool/main/p/pardus-yuzyil/pardus-yuzyil_1.0.0_all.deb
+dpkg --force-all -i /tmp/yuzuncuyil.deb
+sleep 1
+rm /tmp/yuzuncuyil.deb
+
+
+mkdir /var/lib/betikyukleyici
+touch /var/lib/betikyukleyici/yuzuncuyil
+exit 0
---
+++ script/yuzuncuyil-remove.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt remove pardus-yuzyil -y
+
+rm -rf /var/lib/betikyukleyici/yuzuncuyil
+exit 0
