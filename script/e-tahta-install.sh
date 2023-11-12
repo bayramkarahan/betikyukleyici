bb48ddec85106228ebf8755f6eddd082c2d022f6
bayramkarahan <bayramk@gmail.com>
2023-11-12 14:45:56 +0300

    uygulamalarım

--- script/e-tahta-install.sh
+++ script/e-tahta-install.sh
@@ -5,10 +5,11 @@ apt-get install -f -y # eksik bağımlılıkları tamamlaması için.
 apt autoremove -y
 apt update
 ############################################################
-wget -O /tmp/paket.deb https://github.com/bayramkarahan/zamanligorev/raw/master/zamanligorev_1.4.0_amd64.deb
+############################################################
+wget -O /tmp/paket.deb https://github.com/bayramkarahan/e-tahta/raw/master/e-tahta_1.8.8_amd64-debian.deb
 dpkg --force-all -i /tmp/paket.deb
 sleep 1
 rm /tmp/paket.deb
 mkdir /var/lib/betikyukleyici
-touch /var/lib/betikyukleyici/zamanligorev
+touch /var/lib/betikyukleyici/e-tahta
 exit 0
