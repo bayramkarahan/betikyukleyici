5bcf56c7286fc3f97e2303eeff838204aec9588a
karahan <bayramk@gmail.com>
2024-09-26 20:03:13 +0300

    foxit

--- betikyukleyiciscript/foxitreader-install.sh
+++ betikyukleyiciscript/foxitreader-install.sh
@@ -6,10 +6,9 @@ apt autoremove -y
 apt update
 ############################################################
 cd /tmp 
-wget http://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/linux/2.x/2.4/en_us/FoxitReader.enu.setup.2.4.4.0910.x86.run.tar.gz
-tar xzvf FoxitReader*.tar.gz
-chmod a+x FoxitReader*.run
-./FoxitReader*.run
+wget -O https://github.com/bayramkarahan/foxit/raw/refs/heads/master/foxit.run
+chmod a+x foxit.run
+./foxit.run
 
 sleep 1
 
