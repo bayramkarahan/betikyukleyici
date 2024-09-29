89e7c64d4f7c165a3cb739bae904dae79034ac54
karahan <bayramk@gmail.com>
2024-09-29 16:10:09 +0300

    nginx

--- betikyukleyiciscript/e-ag-server-install.sh
+++ betikyukleyiciscript/e-ag-server-install.sh
@@ -6,9 +6,13 @@ apt autoremove -y
 apt update
 ############################################################
 apt remove e-ag-client -y
+apt remove e-ag-x11-client -y
 rm -rf /usr/share/e-ag
-wget -O /tmp/e-ag.deb https://github.com/bayramkarahan/e-ag/raw/refs/heads/master/e-ag_2.2.0_amd64.deb
+wget -O /tmp/e-ag.deb https://github.com/bayramkarahan/e-ag/raw/refs/heads/master/e-ag_2.3.0_amd64.deb
 dpkg --force-all -i /tmp/e-ag.deb
+apt-get install -f -y # eksik bağımlılıkları tamamlaması için.
+apt --fix-broken install -y
+apt autoremove -y
 sleep 1
 mkdir /var/lib/betikyukleyici
 touch /var/lib/betikyukleyici/e-ag
