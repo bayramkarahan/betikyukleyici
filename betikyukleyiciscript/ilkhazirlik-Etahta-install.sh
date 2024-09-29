2d92812ac04b886a5b947d4d3df0410751b358a0
karahan <bayramk@gmail.com>
2024-09-29 22:16:49 +0300

    test

--- betikyukleyiciscript/ilkhazirlik-Etahta-install.sh
+++ betikyukleyiciscript/ilkhazirlik-Etahta-install.sh
@@ -3,12 +3,12 @@ set -ex
 rm -rf /opt/windows/*
 sleep 1
 cd /tmp
-rm -rf /opt/windows/*
+rm -rf /opt/windows/.*
 chown root:root /opt/windows
 ver=$(wine --version|cut -d '-' -f2)
 echo $ver
 wget "https://dl.winehq.org/wine/wine-mono/${ver}.0/wine-mono-${ver}.0-x86.msi"
-WINEPREFIX=/opt/windows wine64 msiexec /i "wine-mono-${ver}.0-x86.msi"
+WINEPREFIX=/opt/windows wine64 msiexec /i "wine-mono-${ver}.0-x86.msi" /qn
 sleep 1
 
 display=":$(ls /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"      #Detect the name of the display in use
