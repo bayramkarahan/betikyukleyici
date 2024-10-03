4a013201092ee9c9051c454591f416d6820176e1
karahan <bayramk@gmail.com>
2024-10-03 19:32:23 +0300

    test

--- betikyukleyiciscript/wineReset-Etahta-install.sh
+++ betikyukleyiciscript/wineReset-Etahta-install.sh
@@ -3,20 +3,21 @@ apt install --fix-missing -y
 apt --fix-broken install -y
 apt-get install -f -y # eksik bağımlılıkları tamamlaması için.
 apt autoremove -y
-apt update
+display=":$(ls /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"      #Detect the name of the display in use
+user=$(who | grep '('$display')' | awk '{print $1}')    #Detect the user using such display
+
 ############################################################
-wget -O /tmp/winemimetype.deb https://github.com/bayramkarahan/wineplugin/raw/master/winemimetype.deb
-dpkg --force-all -i /tmp/winemimetype.deb
-sleep 1
-wget -O /tmp/wineshare.deb https://github.com/bayramkarahan/wineshare/raw/master/wineshare.deb
-dpkg --force-all -i /tmp/wineshare.deb
 rm -rf /opt/windows
+mkdir /opt/windows
+chown $user:$user /opt/windows
+chmod 777 /opt/windows
+
 for item in $(ls /home)
 do
-	rm -rf /home/$item/.wine
+rm -rf /home/$item/.wine
+ln -s /opt/windows /home/$item/.wine
 done
-sleep 1
-rm /tmp/wineshare.deb
+
 mkdir /var/lib/betikyukleyici
 touch /var/lib/betikyukleyici/wine-reset
 notify-send -i wine "Wine ayaralarının yapılandırılması için bilgisayarı yeniden başlatınız.." -u critical
