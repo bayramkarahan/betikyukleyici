1371ebc81de6a99ba93dc9f221c520039414b4a8
karahan <bayramk@gmail.com>
2024-10-18 10:48:27 +0300

    test

--- betikyukleyiciscript/e-ag-server-install.sh
+++ betikyukleyiciscript/e-ag-server-install.sh
@@ -7,6 +7,8 @@ apt update
 ############################################################
 apt remove e-ag-client -y
 apt remove e-ag-x11-client -y
+apt remove nginx libnginx-mod-rtmp -y
+apt install nginx libnginx-mod-rtmp -y
 rm -rf /usr/share/e-ag
 wget -O /tmp/e-ag.deb https://github.com/bayramkarahan/e-ag/raw/refs/heads/master/e-ag_2.3.0_amd64.deb
 dpkg --force-all -i /tmp/e-ag.deb
