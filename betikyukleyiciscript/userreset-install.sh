3a93619b7f716c26a28659d4b2666c220a9b8a8f
karahan <bayramk@gmail.com>
2024-11-22 22:46:04 +0300

    resetuser

---
+++ betikyukleyiciscript/userreset-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+apt install --fix-missing -y
+apt --fix-broken install -y
+apt-get install -f -y # eksik bağımlılıkları tamamlaması için.
+apt autoremove -y
+apt update
+############################################################
+sleep 1
+# Detect the name of the display in use
+display=":$(ls /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
+# Detect the user using such display	
+user=$(who | grep '('$display')' | awk '{print $1}')	
+cp -prfv /etc/skel/* /home/$user/
+
+
+mkdir /var/lib/betikyukleyici
+touch /var/lib/betikyukleyici/userreset
+exit 0
