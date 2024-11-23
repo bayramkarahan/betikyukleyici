bd24af66873486b80ae60fbddc68b9eb3505faa1
karahan <bayramk@gmail.com>
2024-11-23 11:48:34 +0300

    autologin

---
+++ betikyukleyiciscript/otomatikgiris-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+apt install --fix-missing -y
+apt --fix-broken install -y
+apt-get install -f -y # eksik bağımlılıkları tamamlaması için.
+apt autoremove -y
+apt update
+############################################################
+apt-get install zenity -y
+sleep 1
+
+username=$(zenity --entry --title "User Info" --text "Otomatik Giriş Yapılacak Kullanıcıyı Adını Giriniz")
+echo "Girilen isim: $username"
+id $username
+if [ $? -ne 0 ]; then
+	zenity --error --text="Böyle Bir Kullanıcı Yok.\n ${status}" --title="Error" --width=300 --width=200
+	exit 1
+else
+	sed -i -e 's/autologin-user =/#autologin-user =/g' ./lightdm.conf
+	sed -i -e 's/autologin-user=/#autologin-user =/g' ./lightdm.conf
+
+	sed -i 's/\[Seat:\*\]/\[Seat:\*\]\nautologin-user=${username}/g' ./lightdm.conf
+	zenity --info --text="$username Kullanıcısı ile Otomatik Giriş Ayarlandı." --width=400
+fi
+
+mkdir /var/lib/betikyukleyici
+touch /var/lib/betikyukleyici/otomatikgiris
+exit 0
