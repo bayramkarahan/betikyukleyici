42c934b609f892c425d0fd2d4d9a190b2b6c7f62
karahan <bayramk@gmail.com>
2024-12-01 16:05:28 +0300

    test

---
+++ betikyukleyiciscript/smartboard-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+apt install --fix-missing -y
+apt --fix-broken install -y
+apt-get install -f -y # eksik bağımlılıkları tamamlaması için.
+apt autoremove -y
+apt update
+
+apt-get install zenity -y
+sleep 1
+
+############################################################
+wget -O /tmp/app.deb https://github.com/bayramkarahan/rightclicklongpress/releases/download/current/rightclicklongpress_1.0_all.deb
+dpkg --force-all -i /tmp/app.deb
+sleep 1
+rm /tmp/app.deb
+
+glib-compile-schemas /usr/share/glib-2.0/schemas/
+dconf update
+
+
+zenity --info --text="Ayarların geçerli olması için sistemi yeniden başlatınız veya oturumu kapatıp tekrar giriniz." --width=500
+
+mkdir /var/lib/betikyukleyici
+touch /var/lib/betikyukleyici/rightclick
+exit 0
+
---
+++ betikyukleyiciscript/smartboard-remove.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get install zenity -y
+sleep 1
+
+apt remove rightclicklongpress -y
+
+glib-compile-schemas /usr/share/glib-2.0/schemas/
+dconf update
+
+zenity --info --text="Ayarların geçerli olması için sistemi yeniden başlatınız veya oturumu kapatıp tekrar giriniz." --width=500
+
+rm -rf /var/lib/betikyukleyici/rightclick
+exit 0
