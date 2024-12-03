f44cd189fd3565e146cfee49010d05e09cf134d4
karahan <bayramk@gmail.com>
2024-12-03 19:37:58 +0300

    test

--- betikyukleyiciscript/userreset-install.sh
+++ betikyukleyiciscript/userreset-install.sh
@@ -11,7 +11,7 @@ display=":$(ls /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
 # Detect the user using such display	
 user=$(who | grep '('$display')' | awk '{print $1}')	
 cp -prfv /etc/skel/* /home/$user/
-
+chown "${user}:${user}" -R /home/$user/
 
 mkdir /var/lib/betikyukleyici
 touch /var/lib/betikyukleyici/userreset
