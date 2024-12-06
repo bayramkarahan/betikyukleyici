4b011a11a8a486dd3533e8b28d421cedd5d9f004
karahan <bayramk@gmail.com>
2024-12-06 22:36:20 +0300

    test

--- betikyukleyiciscript/mozaik3d-install.sh
+++ betikyukleyiciscript/mozaik3d-install.sh
@@ -11,7 +11,8 @@ display=":$(ls /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
 # Detect the user using such display	
 user=$(who | grep '('$display')' | awk '{print $1}')	
 cd /tmp
-wget --no-check-certificate https://download.mozaweb.com/m3dviewer/m3dViewer_2.0.633_x64.tar.gz
+wget -d --header="User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11" https://download.mozaweb.com/m3dviewer/m3dViewer_2.0.633_x64.tar.gz
+
 tar -xf m3dViewer_2.0.633_x64.tar.gz
 sleep 1
 rm -rf /tmp/m3dViewer_2.0.633_x64.tar.gz
