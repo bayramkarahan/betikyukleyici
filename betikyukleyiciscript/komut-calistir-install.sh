bf668db6754d849d2c06d47e3e6ff8557b94ac89
karahan <bayramk@gmail.com>
2024-12-07 12:06:31 +0300

    test

---
+++ betikyukleyiciscript/komut-calistir-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+apt install --fix-missing -y
+apt --fix-broken install -y
+apt-get install -f -y # eksik bağımlılıkları tamamlaması için.
+apt autoremove -y
+apt update
+############################################################
+apt-get install zenity -y
+sleep 1
+
+komut=$(zenity --entry --title "Komut Çalıştırıcı" --text "Çalıştırılacak Komutu Giriniz"  --width=300)
+echo "Girilen isim: $komut"
+if [ "$komut" != "" ]; then
+	sonuc=$(sh -c "$komut")
+	
+		zenity --info --text="Komut Çalıştırıldı. \n ${sonuc}" --width=400
+fi
+
+mkdir /var/lib/betikyukleyici
+exit 0
