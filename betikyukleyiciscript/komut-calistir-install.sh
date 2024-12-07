3a5d55988d7ab469e8da8abc401df1746206e698
karahan <bayramk@gmail.com>
2024-12-07 12:31:55 +0300

    test

--- betikyukleyiciscript/komut-calistir-install.sh
+++ betikyukleyiciscript/komut-calistir-install.sh
@@ -8,10 +8,10 @@ apt update
 apt-get install zenity -y
 sleep 1
 
-komut=$(zenity --entry --title "Komut Çalıştırıcı" --text "Çalıştırılacak Komutu Giriniz"  --width=300)
+komut=$(zenity --entry --title "Komut Çalıştırıcı" --text "Çalıştırılacak Komutu Giriniz"  --width=400)
 echo "Girilen isim: $komut"
 if [ "$komut" != "" ]; then
-	sonuc=$(sh -c "env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY  $komut")
+	sonuc=$(sh -c "$komut")
 	
 		zenity --info --text="Komut Çalıştırıldı. \n ${sonuc}" --width=400
 fi
