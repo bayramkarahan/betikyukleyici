015de63dac8108d028cf3ce6f2f85ab3c574407c
bayram <bayramk@gmail.com>
2025-02-09 20:14:09 +0300

    test

--- betikyukleyiciscript/wine-install.sh
+++ betikyukleyiciscript/wine-install.sh
@@ -6,42 +6,47 @@ apt autoremove -y
 apt update
 ############################################################
 #*******************************************************************************************
-wget -O /tmp/winehq.key https://dl.winehq.org/wine-builds/winehq.key
-apt update
-dpkg --add-architecture i386
+# paket yönetim paketi add-apt-repository bu paketten gelir
 apt install software-properties-common -y
-apt-key add /tmp/winehq.key
-echo 'deb https://dl.winehq.org/wine-builds/debian/ bullseye main'> /etc/apt/sources.list.d/winehq.list
-apt update
-#apt install --install-recommends winehq-devel -y
-apt install --install-recommends winehq-stable -y
-rm -rf /tmp/winehq.key
-#*******************************************************************************************
-apt install libgtk3-nocsd0:i386 -y
+# şifreleme ve dijital imzalama aracıdır.
 apt install gnupg2 -y
-##apt install software-properties-common -y
-apt install winetricks -y 
+# Bu komut, sisteminize 32-bit (i386) mimarisini ekler
+dpkg --add-architecture i386
+
+#genel wine keyi ekleniyor
+wget -O /tmp/winehq.key https://dl.winehq.org/wine-builds/winehq.key
+apt-key add /tmp/winehq.key
+
+#key ekleniyor
+mkdir -pm755 /etc/apt/keyrings
+rm -rf /etc/apt/keyrings/winehq*
+wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key -
+#repostory ekleniyor
+echo 'deb https://dl.winehq.org/wine-builds/debian/ bookworm main'> /etc/apt/sources.list.d/winehq.list
+apt update
+apt install winehq-stable=9.0.0.0~bookworm-1 wine-stable=9.0.0.0~bookworm-1 wine-stable-amd64=9.0.0.0~bookworm-1 wine-stable-i386=9.0.0.0~bookworm-1 -y
+
+#Windows uygulamalarının (DLL dosyaları vb.) otomatik olarak indirip kurabilir
+apt install --no-install-recommends winetricks -y
+# .NET uygulamalarını Linux üzerinde wine ile çalıştırmak için gerekli
 apt install mono-complete -y
-#*******************************************************************************************
+apt install libgtk3-nocsd0:i386 -y
+
+rm -rf /tmp/winemimetype.deb
 wget -nc -O /tmp/winemimetype.deb https://github.com/bayramkarahan/wineplugin/raw/master/winemimetype.deb
 sudo dpkg -i --force-all /tmp/winemimetype.deb
 
+rm -rf /tmp/wineshare.deb
 wget -nc -O /tmp/wineshare.deb https://github.com/bayramkarahan/wineshare/raw/master/wineshare.deb
 sudo dpkg -i --force-all /tmp/wineshare.deb
-apt install -f -y
+
 
 apt autoremove -y
 apt autoclean -y
+rm -rf /etc/apt/sources.list.d/winehq*
+rm -rf /etc/apt/keyrings/wine*
 apt update
 
-sleep 1
-rm /tmp/winemimetype.deb
-mkdir /var/lib/betikyukleyici
-touch /var/lib/betikyukleyici/winemimetype
 
-apt autoremove -y
-apt autoclean -y
-
-rm -rf /etc/apt/sources.list.d/winehq.list
 touch /var/lib/betikyukleyici/wine
 exit 0
