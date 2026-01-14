e8a588d8ca31853aaf5635319c343a78b39404d9
bayram <bayramk@gmail.com>
2026-01-14 22:08:54 +0300

    test

--- betikyukleyiciscript/update-install.sh
+++ betikyukleyiciscript/update-install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/lib/dpkg/lock
+sudo rm /var/cache/apt/archives/lock
+sudo dpkg --configure -a
 apt install --fix-missing -y
 apt --fix-broken install -y
 apt-get install -f -y # eksik bağımlılıkları tamamlaması için.
